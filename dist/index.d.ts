75487414891a1c6875a72384050a18b5bd15eada
Toby Zerner <toby.zerner@gmail.com>
2021-03-31 11:47:53 +1030

    Compile, add release-it

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,6 +3,7 @@ declare type TransitionOptions = {
     prefix?: string;
 };
 export declare function move(elements: HTMLCollection | HTMLElement[], cb: Function, options?: TransitionOptions): void;
+export declare function cancel(el: HTMLElement): void;
 export declare function transition(el: HTMLElement, name: string, options?: TransitionOptions): void;
 export declare function hello(el: HTMLElement, options?: TransitionOptions): void;
 export declare function goodbye(el: HTMLElement, options?: TransitionOptions): void;
